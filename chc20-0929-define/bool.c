a9e0ffc15e3d6488eec4cffbe5d7b10f5ffc879f
ARFNeto-CH <54643794+ARFNeto-CH@users.noreply.github.com>
2020-09-29 15:49:05 -0300

    Add project files.

---
+++ chc20-0929-define/bool.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include "coisa.xls"
+
+int main(void)
+{
+        #include "mario."
+
+    printf("%s ", bool(not false) );
+    printf("%s ", bool(false and true));
+    printf("%s ", bool(true or false));
+
+    int ret =
+#include "britivaldo."
+        ;
+
+    printf("\n\nVai retornar com '%d'\n", ret);
+        return ret;
+        ;
+};
